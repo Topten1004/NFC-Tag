bd4882c703e15193111cf73fffec8b195feec23d
Dmytro Kulakov <dmytrokulakovdev@outlook.com>
2024-09-12 06:03:20 +0200

    updated the auth part

--- ClinoTagWeb/BqsClinoTag/Hubs/ChatHub.cs
+++ ClinoTagWeb/BqsClinoTag/Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BqsClinoTag.Models;
+using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
 using RestSharp;
 using System;
@@ -8,6 +9,17 @@ using System.Threading.Tasks;
 public class ChatHub : Hub
 {
     private static Dictionary<string, List<string>> _rooms = new Dictionary<string, List<string>>();
+
+    private readonly CLINOTAGBQSContext _db;
+    private readonly IConfiguration _configuration;
+    private readonly string _apiKey;
+
+    public ChatHub(CLINOTAGBQSContext db, IConfiguration configuration)
+    {
+        _db = db;
+        _configuration = configuration;
+        _apiKey = _configuration["OpenAiSettings:ApiKey"];
+    }
 
     public async Task SendMessageToRoom(string roomId, string message)
     {
@@ -20,7 +32,17 @@ public class ChatHub : Hub
         }
         _rooms[roomId].Add(message);
 
+        ChatHistory chatHistory = new ChatHistory();
+
         var translation = await TranslateTextAsync(message);
+
+        chatHistory.CreatedTime = DateTime.Now;
+        chatHistory.RoomName = roomId;
+        chatHistory.Content = message;
+        chatHistory.Checked = true;
+
+        _db.ChatHistorys.Add(chatHistory);
+        await _db.SaveChangesAsync();
 
         // Broadcast message to all clients in the room
         await Clients.Group(roomId).SendAsync("ReceiveMessage", new { roomId, text = translation });
@@ -57,6 +79,7 @@ public class ChatHub : Hub
         {
             return _rooms[roomId];
         }
+
         return new List<string>();
     }
 
@@ -66,9 +89,20 @@ public class ChatHub : Hub
         await Clients.Caller.SendAsync("ReceiveRoomList", rooms);
     }
 
+    public static List<string> GetMessages(string roomId, CLINOTAGBQSContext db)
+    {
+        var messages = db.ChatHistorys
+                         .Where(ch => ch.RoomName == roomId)
+                         .OrderBy(ch => ch.CreatedTime)
+                         .Select(ch => ch.Content)
+                         .ToList();
+
+        return messages;
+    }
+
     public async Task GetRoomMessages(string roomId)
     {
-        var messages = GetMessages(roomId);
+        var messages = GetMessages(roomId, _db);
         await Clients.Caller.SendAsync("ReceiveRoomMessages", new { roomId, messages });
     }
 
